e9ec82e39b9b87ad5944e6b5ae1270853e44b03a
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-07-10 00:20:57 +0200

    init http client
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,66 @@
 #![deny(missing_debug_implementations, nonstandard_style)]
 #![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
 #![cfg_attr(test, deny(warnings))]
+#![feature(async_await)]
+
+use std::fmt::Debug;
+
+pub use http;
+
+/// A generic error type.
+pub type Fail = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+/// Create an HTTP test.
+#[derive(Debug)]
+pub struct Client {
+    client: hyper::client::Builder,
+    method: http::Method,
+    uri: http::Uri,
+    body: hyper::Body,
+}
+
+impl Client {
+    /// Create a new instance.
+    pub fn new(method: http::Method, uri: http::Uri) -> Self {
+        Self {
+            client: hyper::client::Client::builder(),
+            uri,
+            method,
+            body: hyper::Body::empty(),
+        }
+    }
+
+    /// Send a request and format the response as a `String`.
+    pub async fn send_text(mut self) -> Result<Response<String>, Fail> {
+        let req = hyper::Request::builder()
+            .method(self.method)
+            .uri(self.uri)
+            .body(self.body)?;
+        let client = hyper::Client::new();
+        let res = client.request(req).await?;
+        unimplemented!();
+    }
+}
+
+/// Surf response
+#[derive(Debug)]
+pub struct Response<Body: Debug> {
+    body: Body,
+}
+
+impl<Body: Debug> Response<Body> {
+    /// Create a new instance
+    fn new() -> Self {
+        unimplemented!();
+    }
+
+    /// Get the body.
+    pub fn body(self) -> Body {
+        self.body
+    }
+}
+
+/// Perform a `GET` request.
+pub fn get(uri: http::Uri) -> Client {
+    Client::new(http::Method::GET, uri)
+}
