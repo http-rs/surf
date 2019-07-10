9e3ee4061b5492031141431f3fbec0524f8a7eab
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-07-10 03:01:18 +0200

    placeholders
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- examples/google.rs
+++ examples/google.rs
@@ -4,7 +4,7 @@ type Fail = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[runtime::main(runtime_tokio::Tokio)]
 async fn main() -> Result<(), Fail>{
-    let text = surf::get("http://google.com").send_text().await?;
+    let text = surf::get("http://google.com").string().await?;
     dbg!(text);
     Ok(())
 }
