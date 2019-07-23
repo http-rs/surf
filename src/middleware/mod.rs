4960500830e7110e739b675e719e16fd1c9c2614
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-07-24 00:14:47 +0200

    tests pass again
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -4,23 +4,24 @@
 //! ```
 //! # #![feature(async_await)]
 //! use futures::future::BoxFuture;
-//! use surf::middleware::{Next, Middleware, Request, Response};
+//! use surf::middleware::{Next, Middleware, Request, Response, HttpClient};
 //! use std::time;
 //!
 //! /// Log each request's duration
 //! #[derive(Debug)]
 //! pub struct Logger;
 //!
-//! impl Middleware for Logger {
+//! impl<C: HttpClient> Middleware<C> for Logger {
 //!     fn handle<'a>(
 //!         &'a self,
 //!         req: Request,
-//!         next: Next<'a>,
+//!         client: C,
+//!         next: Next<'a, C>,
 //!     ) -> BoxFuture<'a, Result<Response, surf::Exception>> {
 //!         Box::pin(async move {
 //!             println!("sending request to {}", req.uri());
 //!             let now = time::Instant::now();
-//!             let res = next.run(req).await?;
+//!             let res = next.run(req, client).await?;
 //!             println!("request completed ({:?})", now.elapsed());
 //!             Ok(res)
 //!         })
@@ -33,14 +34,14 @@
 //! ```
 //! # #![feature(async_await)]
 //! use futures::future::BoxFuture;
-//! use surf::middleware::{Next, Middleware, Request, Response};
+//! use surf::middleware::{Next, Middleware, Request, Response, HttpClient};
 //! use std::time;
 //!
-//! fn logger<'a>(req: Request, next: Next<'a>) -> BoxFuture<'a, Result<Response, surf::Exception>> {
+//! fn logger<'a, C: HttpClient>(req: Request, client: C, next: Next<'a, C>) -> BoxFuture<'a, Result<Response, surf::Exception>> {
 //!     Box::pin(async move {
 //!         println!("sending request to {}", req.uri());
 //!         let now = time::Instant::now();
-//!         let res = next.run(req).await?;
+//!         let res = next.run(req, client).await?;
 //!         println!("request completed ({:?})", now.elapsed());
 //!         Ok(res)
 //!     })
@@ -48,7 +49,7 @@
 //! ```
 
 #[doc(inline)]
-pub use crate::http_client::{Request, Response, HttpClient};
+pub use crate::http_client::{HttpClient, Request, Response};
 
 pub mod logger;
 
--- src/one_off.rs
+++ src/one_off.rs
@@ -55,19 +55,23 @@ pub fn head(uri: impl AsRef<str>) -> Request<HyperClient> {
 /// this case, the content type is selected by putting the adequate string in the enctype attribute
 /// of the <form> element or the formenctype attribute of the <input> or <button> elements:
 ///
-///     application/x-www-form-urlencoded: the keys and values are encoded in key-value tuples separated by '&', with a '=' between the key and the value. Non-alphanumeric characters in both keys and values are percent encoded: this is the reason why this type is not suitable to use with binary data (use multipart/form-data instead)
-///     multipart/form-data: each value is sent as a block of data ("body part"), with a user agent-defined delimiter ("boundary") separating each part. The keys are given in the Content-Disposition header of each part.
-///     text/plain
+/// ```txt
+/// application/x-www-form-urlencoded: the keys and values are encoded in key-value tuples separated by '&', with a '=' between the key and the value. Non-alphanumeric characters in both keys and values are percent encoded: this is the reason why this type is not suitable to use with binary data (use multipart/form-data instead)
+/// multipart/form-data: each value is sent as a block of data ("body part"), with a user agent-defined delimiter ("boundary") separating each part. The keys are given in the Content-Disposition header of each part.
+/// text/plain
+/// ```
 ///
 /// When the POST request is sent via a method other than an HTML form — like via an XMLHttpRequest
 /// — the body can take any type. As described in the HTTP 1.1 specification, POST is designed to
 /// allow a uniform method to cover the following functions:
 ///
-///     Annotation of existing resources
-///     Posting a message to a bulletin board, newsgroup, mailing list, or similar group of articles;
-///     Adding a new user through a signup modal;
-///     Providing a block of data, such as the result of submitting a form, to a data-handling process;
-///     Extending a database through an append operation.
+/// ```txt
+/// Annotation of existing resources
+/// Posting a message to a bulletin board, newsgroup, mailing list, or similar group of articles;
+/// Adding a new user through a signup modal;
+/// Providing a block of data, such as the result of submitting a form, to a data-handling process;
+/// Extending a database through an append operation.
+/// ```
 ///
 /// [Read more on MDN]
 ///
