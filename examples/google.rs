55377da781434d73146f978eb5799ec0e9d30ce0
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-07-24 20:05:19 +0200

    update example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- examples/google.rs
+++ examples/google.rs
@@ -1,7 +1,8 @@
 #![feature(async_await)]
 type Exception = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-#[runtime::main]
+#[runtime::main(runtime_tokio::Tokio)]
+// #[runtime::main]
 async fn main() -> Result<(), Exception> {
     let string = surf::get("https://google.com")
         .middleware(surf::middleware::logger::new())
