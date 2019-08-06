f7e73d926c9d9ef333b5f1777bf8120f13e834fa
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-08-06 11:56:55 +0200

    requests work!
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/http_client/wasm.rs
+++ src/http_client/wasm.rs
@@ -2,6 +2,7 @@ use super::{Body, HttpClient, Request, Response};
 
 use futures::future::BoxFuture;
 use futures::prelude::*;
+use js_sys::{ArrayBuffer, Uint8Array};
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::futures_0_3::JsFuture;
 use web_sys::window;
@@ -34,17 +35,31 @@ impl HttpClient for WasmClient {
 
     fn send(&self, req: Request) -> BoxFuture<'static, Result<Response, Self::Error>> {
         let fut = Box::pin(async move {
-            let url = format!("{}", req.uri());
-            let request = web_sys::Request::new_with_str(&url).unwrap();
-
             let window = window().expect("A global window object could not be found");
+            let url = format!("{}", req.uri());
+
+            // Do a request
+            let request = web_sys::Request::new_with_str(&url).unwrap();
             let promise = window.fetch_with_request(&request);
+            let resp = JsFuture::from(promise).await.unwrap();
+            debug_assert!(resp.is_instance_of::<web_sys::Response>());
+            let res: web_sys::Response = resp.dyn_into().unwrap();
 
-            let res = JsFuture::from(promise).await.unwrap();
-            debug_assert!(res.is_instance_of::<web_sys::Response>());
+            // Get the body
+            let promise = res.array_buffer().unwrap();
+            let resp = JsFuture::from(promise).await.unwrap();
+            debug_assert!(resp.is_instance_of::<js_sys::ArrayBuffer>());
+            let buf: ArrayBuffer = resp.dyn_into().unwrap();
+            let slice = Uint8Array::new(&buf);
+            let mut dest: Vec<u8> = vec![0; slice.length() as usize];
+            slice.copy_to(&mut dest);
 
-            let res: web_sys::Response = res.dyn_into().unwrap();
-            Ok(Response::new(Body::empty()))
+            // Create our response
+            let _headers = res.headers();
+            let mut response = Response::new(Body::from(dest));
+            *response.status_mut() = http::StatusCode::from_u16(res.status()).unwrap();
+
+            Ok(response)
         });
 
         Box::pin(InnerFuture { fut })
