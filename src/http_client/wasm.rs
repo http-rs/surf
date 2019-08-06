ee54dee51cd18849d12f02b8af5206a5b1269f53
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-08-07 01:47:31 +0200

    split out fetch code to separate block
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/http_client/wasm.rs
+++ src/http_client/wasm.rs
@@ -2,7 +2,7 @@ use super::{Body, HttpClient, Request, Response};
 
 use futures::future::BoxFuture;
 use futures::prelude::*;
-use js_sys::{ArrayBuffer, Uint8Array};
+use js_sys::{Array, ArrayBuffer, Uint8Array};
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::futures_0_3::JsFuture;
 use web_sys::window;
@@ -35,31 +35,22 @@ impl HttpClient for WasmClient {
 
     fn send(&self, req: Request) -> BoxFuture<'static, Result<Response, Self::Error>> {
         let fut = Box::pin(async move {
-            let window = window().expect("A global window object could not be found");
             let url = format!("{}", req.uri());
+            let req = fetch::new(req.method().as_str(), &url);
+            let mut res = req.send().await?;
 
-            // Do a request
-            let mut init = web_sys::RequestInit::new();
-            init.method(req.method().as_str());
-            let request = web_sys::Request::new_with_str_and_init(&url, &init).unwrap();
-            let promise = window.fetch_with_request(&request);
-            let resp = JsFuture::from(promise).await.unwrap();
-            debug_assert!(resp.is_instance_of::<web_sys::Response>());
-            let res: web_sys::Response = resp.dyn_into().unwrap();
-
-            // Get the body
-            let promise = res.array_buffer().unwrap();
-            let resp = JsFuture::from(promise).await.unwrap();
-            debug_assert!(resp.is_instance_of::<js_sys::ArrayBuffer>());
-            let buf: ArrayBuffer = resp.dyn_into().unwrap();
-            let slice = Uint8Array::new(&buf);
-            let mut dest: Vec<u8> = vec![0; slice.length() as usize];
-            slice.copy_to(&mut dest);
-
-            // Create our response
-            let _headers = res.headers();
-            let mut response = Response::new(Body::from(dest));
+            let body = res.body_bytes();
+            let mut response = Response::new(Body::from(body));
             *response.status_mut() = http::StatusCode::from_u16(res.status()).unwrap();
+
+            // let headers = js_sys::try_iter(&res.headers()).unwrap().flatten();
+            // for pair in headers {
+            //     let array: Array = pair.unwrap().into();
+            //     let vals = array.values();
+            //     let key = vals.next().unwrap();
+            //     let value = vals.next().unwrap();
+            //     web_sys::console::log_2(&key, &value);
+            // }
 
             Ok(response)
         });
@@ -85,5 +76,120 @@ impl Future for InnerFuture {
         // future, in order to implement `Send`. If it's safe to poll the inner future, it's safe
         // to proxy it too.
         unsafe { Pin::new_unchecked(&mut self.fut).poll(cx) }
+    }
+}
+
+mod fetch {
+    use futures::future::BoxFuture;
+    use futures::prelude::*;
+    use js_sys::{Array, ArrayBuffer, Uint8Array};
+    use wasm_bindgen::JsCast;
+    use wasm_bindgen_futures::futures_0_3::JsFuture;
+    use web_sys::window;
+    use web_sys::RequestInit;
+
+    use std::iter::Iterator;
+    use std::io;
+
+    /// Create a new fetch request.
+    pub(crate) fn new(method: impl AsRef<str>, url: impl AsRef<str>) -> Request {
+        Request::new(method, url)
+    }
+
+    /// An HTTP Fetch Request.
+    pub(crate) struct Request {
+        init: RequestInit,
+        url: String,
+    }
+
+    impl Request {
+        /// Create a new instance.
+        pub(crate) fn new(method: impl AsRef<str>, url: impl AsRef<str>) -> Self {
+            let mut init = web_sys::RequestInit::new();
+            init.method(method.as_ref());
+            Self {
+                init,
+                url: url.as_ref().to_owned(),
+            }
+        }
+
+        /// Submit a request
+        // TODO(yoshuawuyts): turn this into a `Future` impl on `Request` instead.
+        pub(crate) async fn send(self) -> Result<Response, io::Error> {
+            // Send the request.
+            let window = window().expect("A global window object could not be found");
+            let request = web_sys::Request::new_with_str_and_init(&self.url, &self.init).unwrap();
+            let promise = window.fetch_with_request(&request);
+            let resp = JsFuture::from(promise).await.unwrap();
+            debug_assert!(resp.is_instance_of::<web_sys::Response>());
+            let res: web_sys::Response = resp.dyn_into().unwrap();
+
+            // Get the request body.
+            let promise = res.array_buffer().unwrap();
+            let resp = JsFuture::from(promise).await.unwrap();
+            debug_assert!(resp.is_instance_of::<js_sys::ArrayBuffer>());
+            let buf: ArrayBuffer = resp.dyn_into().unwrap();
+            let slice = Uint8Array::new(&buf);
+            let mut body: Vec<u8> = vec![0; slice.length() as usize];
+            slice.copy_to(&mut body);
+
+            Ok(Response { res, body: Some(body) })
+        }
+    }
+
+    /// An HTTP Fetch Response.
+    pub(crate) struct Response {
+        res: web_sys::Response,
+        body: Option<Vec<u8>>,
+    }
+
+    impl Response {
+        fn new(res: web_sys::Response, body: Vec<u8>) -> Self {
+            Self {
+                res,
+                body: Some(body),
+            }
+        }
+
+        /// Access the HTTP headers.
+        pub(crate) fn headers(&self) -> &Headers<'_> {
+            // &Headers {
+            //     res: self,
+            //     iter: js_sys::try_iter(&self.res.headers()).unwrap().unwrap().flatten(),
+            // }
+            unimplemented!();
+        }
+
+        /// Get the request body as a byte vector.
+        ///
+        /// Returns an empty vector if the body has already been consumed.
+        pub(crate) fn body_bytes(&mut self) -> Vec<u8> {
+            self.body.take().unwrap_or_else(|| vec![])
+        }
+
+        /// Get the HTTP return status code.
+        pub(crate) fn status(&self) -> u16 {
+            self.res.status()
+        }
+    }
+
+    /// HTTP Headers.
+    pub(crate) struct Headers<'a> {
+        res: &'a Response,
+        iter: (),
+    }
+
+    impl Iterator for Headers<'_> {
+        type Item = (String, String);
+
+        fn next(&mut self) -> Option<Self::Item> {
+            // let pair = self.iter.next()?;
+            // let array: Array = pair.unwrap().into();
+            // let vals = array.values();
+            // let key = vals.next().unwrap();
+            // let value = vals.next().unwrap();
+            // Some((key, value))
+            unimplemented!();
+        }
     }
 }
