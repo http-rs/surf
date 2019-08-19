47a43dc9039d1a7035ce0b209cb145034292305c
Artem Vorotnikov <artem@vorotnikov.me>
2019-08-20 00:18:44 +0300

    cHTTP has been renamed to isahc

--- src/http_client/native.rs
+++ src/http_client/native.rs
@@ -1,5 +1,5 @@
 #[cfg(all(feature = "curl-client", not(target_arch = "wasm32")))]
-pub(crate) use super::chttp::ChttpClient as NativeClient;
+pub(crate) use super::isahc::IsahcClient as NativeClient;
 
 #[cfg(all(feature = "wasm-client", target_arch = "wasm32"))]
 pub(crate) use super::wasm::WasmClient as NativeClient;
