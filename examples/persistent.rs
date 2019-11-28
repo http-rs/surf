7bbf4292beb1ec74ef88a7db8b1aaaa91cae232e
Richard Dodd <richard.o.dodd@gmail.com>
2019-11-28 23:38:00 +0000

    Make all examples use generic error.

--- examples/persistent.rs
+++ examples/persistent.rs
@@ -1,14 +1,16 @@
 use async_std::task;
 
+type BoxError = Box<dyn std::error::Error + Send + Sync>;
+
 // The need for Ok with turbofish is explained here
 // https://rust-lang.github.io/async-book/07_workarounds/03_err_in_async_blocks.html
-fn main() -> Result<(), surf::Error> {
-    femme::start(log::LevelFilter::Info).unwrap();
+fn main() -> Result<(), BoxError> {
+    femme::start(log::LevelFilter::Info)?;
     task::block_on(async {
         let client = surf::Client::new();
         let req1 = client.get("https://httpbin.org/get").recv_string();
         let req2 = client.get("https://httpbin.org/get").recv_string();
         futures::future::try_join(req1, req2).await?;
-        Ok::<(), surf::Error>(())
+        Ok(())
     })
 }
--- examples/post.rs
+++ examples/post.rs
@@ -1,14 +1,15 @@
 use async_std::task;
 
-// The need for Ok with turbofish is explained here
-// https://rust-lang.github.io/async-book/07_workarounds/03_err_in_async_blocks.html
-fn main() -> Result<(), surf::Error> {
-    femme::start(log::LevelFilter::Info).unwrap();
+type BoxError = Box<dyn std::error::Error + Send + Sync>;
+
+fn main() -> Result<(), BoxError> {
+    femme::start(log::LevelFilter::Info)?;
     task::block_on(async {
         let uri = "https://httpbin.org/post";
         let data = serde_json::json!({ "name": "chashu" });
+        // unwrap note: we are definitely passing valid json so we should never panic.
         let res = surf::post(uri).body_json(&data).unwrap().await?;
         assert_eq!(res.status(), 200);
-        Ok::<(), surf::Error>(())
+        Ok(())
     })
 }
