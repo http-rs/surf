1adb82125268371fa6eb224ac9c79cedd20dc9bb
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2021-03-01 11:55:31 -0800

    Add 'h1-client-rustls' feature relying on rustls
    This PR is a follow-up of http-rs/http-client#53 and addresses #40.
    It adds a `h1-client-rustls` feature using `http-client/rustls` feature introduced by http-rs/http-client#53.
    
    Co-Authored-By: Julien Enoch <julien.enoch@adlinktech.com>

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,8 @@
 //! The following features are available. The default features are
 //! `curl-client`, `middleware-logger`, and `encoding`
 //! - __`curl-client` (default):__ use `curl` (through `isahc`) as the HTTP backend.
-//! - __`h1-client`:__ use `async-h1` as the HTTP backend.
+//! - __`h1-client`:__ use `async-h1` as the HTTP backend with native TLS for HTTPS.
+//! - __`h1-client-rustls`:__ use `async-h1` as the HTTP backend with `rustls` for HTTPS.
 //! - __`hyper-client`:__ use `hyper` (hyper.rs) as the HTTP backend.
 //! - __`wasm-client`:__ use `window.fetch` as the HTTP backend.
 //! - __`middleware-logger` (default):__ enables logging requests and responses using a middleware.
